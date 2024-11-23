49dc592e1efaba468af38e23ea33d77b3e124296
Md. Refayatul Islam <refayatul7@gmail.com>
2024-11-23 14:58:24 +0600

    made some more changes

--- lib/four_rakahs_of_sunnah.dart
+++ lib/four_rakahs_of_sunnah.dart
@@ -5,6 +5,25 @@ class FourRakahsOfSunnah extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("4 Rak'as of Fard"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Learn to Pray 4 Rak'as of Fard"),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Return'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
